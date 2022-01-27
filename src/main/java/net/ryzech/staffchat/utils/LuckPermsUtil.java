9fa62506da0d0b00edd11d4e5663903e887ea404
RyzechDev // Ryz <61711965+ryzech@users.noreply.github.com>
2022-01-27 14:57:31 -0600

    fix non-static luckperms var

--- src/main/java/net/ryzech/staffchat/utils/LuckPermsUtil.java
+++ src/main/java/net/ryzech/staffchat/utils/LuckPermsUtil.java
@@ -7,7 +7,7 @@ import net.luckperms.api.model.user.User;
 import net.ryzech.staffchat.StaffChat;
 
 public class LuckPermsUtil {
-    LuckPerms luckPerms = StaffChat.getInstance().getLuckPermsProvider();
+    private static LuckPerms luckPerms = StaffChat.getInstance().getLuckPermsProvider();
     public static String getPrefix(Player player) 
     {
         User user = luckPerms.getUserManager().getUser(player.getUniqueId());
